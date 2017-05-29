e041fe3869be3edb20ec724c7ded1eed3385facb
NullVoxPopuli <LPSego3+dev@gmail.com>
2017-05-28 21:51:09 -0400

    default export first

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { stateWrapper } from './state-wrapper';
 
+export default stateWrapper;
+
 export { withValue } from './withValue';
 export { findValue } from './findValue';
 export { mutCreator } from './mut';
 export { toggleCreator } from './toggle';
 export { handleSumbit } from './handleSubmit';
-
-export default stateWrapper;
